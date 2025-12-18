910343d935a44dcf375d84fb18249ea73a89f685
edipo <edipo@viasoft.com.br>
2025-12-18 08:35:48 -0300

    Implementa método para salvar e carregar arquivo JSON

--- src/main/java/br/com/dominiodev/contador_de_historias/Criador.java
+++ src/main/java/br/com/dominiodev/contador_de_historias/Criador.java
@@ -1,12 +1,20 @@
 package br.com.dominiodev.contador_de_historias;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Criador {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        Interacao interacaoInicial = criarInteracao(sc);
+        System.out.println("Menu inicial");
+        System.out.println("[1] Iniciar nova história");
+        System.out.println("[2] Carregar existente");
+        System.out.println("Digite sua escolha: ");
+        int escolha = Integer.parseInt(sc.nextLine());
+        Interacao interacaoInicial = escolha == 1 ? criarInteracao(sc) : carrecarInteracaoInicial(sc);
         mostrarMenu(sc,  interacaoInicial);
     }
 
@@ -28,12 +36,13 @@ public class Criador {
         Interacao interacaoSelecionada = interacaoInicial;
         while (menuSelecionado != 0) {
             System.out.println("Menu");
-            System.out.println("0. Fechar");
-            System.out.println("1. Lista de interacoes");
-            System.out.println("2. Entrar na interacao");
-            System.out.println("3. Sair da interacao");
-            System.out.println("4. Incluir interacao");
-            System.out.println("5. Excluir interacao");
+            System.out.println("[0] Fechar");
+            System.out.println("[1] Lista de interacoes");
+            System.out.println("[2] Entrar na interacao");
+            System.out.println("[3] Sair da interacao");
+            System.out.println("[4] Incluir interacao");
+            System.out.println("[5] Excluir interacao");
+            System.out.println("[6] Salvar historia");
             System.out.println("Digite sua escolha: ");
             menuSelecionado = Integer.parseInt(sc.nextLine());
 
@@ -47,12 +56,19 @@ public class Criador {
                 incluirInteracao(sc, interacaoSelecionada);
             } else if (menuSelecionado == 5) {
                 excluirInteracao(sc, interacaoSelecionada);
+            } else if (menuSelecionado == 6) {
+                salvarHistoria(sc, interacaoInicial);
             }
         }
     }
 
     private static void listaDeInteracoes(Interacao interacaoSelecionada) {
         System.out.println("Lista de interacoes");
+        if (interacaoSelecionada.getOpcoesParaEscolha().isEmpty()) {
+            System.out.println("=================================================");
+            System.out.println("Nenhuma interacao foi encontrada");
+            System.out.println("=================================================");
+        }
         interacaoSelecionada.getOpcoesParaEscolha().forEach(interacao -> {
             System.out.println("=================================================");
             System.out.println("Código: " + interacaoSelecionada.getOpcoesParaEscolha().indexOf(interacao));
@@ -64,18 +80,23 @@ public class Criador {
 
     private static Interacao entrarNaInteracao(Scanner sc, Interacao interacaoSelecionada) {
         listaDeInteracoes(interacaoSelecionada);
+        if (interacaoSelecionada.getOpcoesParaEscolha().isEmpty()) {
+            return interacaoSelecionada;
+        }
         System.out.println("Digite o código da interacao: ");
         int codigoInteracao = Integer.parseInt(sc.nextLine());
         return interacaoSelecionada.getOpcoesParaEscolha().get(codigoInteracao);
     }
 
     private static Interacao sairDaInteracao(Interacao interacaoInicial) {
+        System.out.println("=================================================");
         System.out.println("Retornando para a interacao inicial.");
+        System.out.println("=================================================");
         return interacaoInicial;
     }
 
     private static void incluirInteracao(Scanner sc, Interacao interacaoSelecionada) {
-        System.out.println("Nota interacao");
+        System.out.println("Nova interacao");
         Interacao novaInteracao = criarInteracao(sc);
         interacaoSelecionada.adicionarInteracao(novaInteracao);
         System.out.println("Interacao incluida com sucesso!");
@@ -83,9 +104,40 @@ public class Criador {
 
     private static void excluirInteracao(Scanner sc, Interacao interacaoSelecionada) {
         listaDeInteracoes(interacaoSelecionada);
+        if (interacaoSelecionada.getOpcoesParaEscolha().isEmpty()) {
+            return;
+        }
         System.out.println("Digite o código da interacao que deseja excluir: ");
         int codigoInteracao = Integer.parseInt(sc.nextLine());
         interacaoSelecionada.excluirInteracao(codigoInteracao);
         System.out.println("Interacao excluida com sucesso!");
+    }
+
+    private static void salvarHistoria(Scanner sc, Interacao interacaoInicial) {
+        try {
+            System.out.println("Informe o nome do arquivo que será salvo: ");
+            String nomeArquivo = sc.nextLine();
+
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(nomeArquivo + ".json"), interacaoInicial);
+
+            System.out.println("Historia salvo com sucesso!");
+        } catch (Exception e) {
+            System.out.println("Erro ao salvar historia: " + e.getMessage());
+        }
+    }
+
+    private static Interacao carrecarInteracaoInicial(Scanner sc) throws Exception {
+        try {
+            System.out.println("Informe o nome do arquivo que deseja carregar: ");
+            String nomeArquivo = sc.nextLine();
+            ObjectMapper mapper = new ObjectMapper();
+            Interacao interacaoInicial = mapper.readValue(new File(nomeArquivo + ".json"), Interacao.class);
+            System.out.println("Interação carregada com sucesso!");
+            return  interacaoInicial;
+        } catch (Exception e) {
+            System.out.println("Erro ao carregar interacao: " + e.getMessage());
+            throw e;
+        }
     }
 }
